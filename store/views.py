df565e03b1a8a4a3bc27cf358fd9432e1583985a
gkvartskhava <gio.kvartskhava@gmail.com>
2024-10-07 17:12:44 +0200

    first

--- store/views.py
+++ store/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from .models import Book, Author
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from .serializers import AuthorSerializer, BookSerializer
+from rest_framework import permissions
 
-# Create your views here.
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    # permission_classes = [permissions.IsAdminUser]
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class CustomAuthToken(APIView):
+
+    def post(self,request,*args,**kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password = password)
+        if user:
+            token,created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error':'Invalid Login or Password'}, status=status.HTTP_400_BAD_REQUEST )
